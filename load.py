fc7d52d6bc1be2b5c857d7138795d182845718f1
lsylyx <lsy6532951@qq.com>
2026-06-26 14:24:38 +0800

    add dataloader

---
+++ load.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import shutil
+from argparse import ArgumentParser
+
+def parse_args():
+    parser = ArgumentParser(description="Load dataset.")
+    parser.add_argument("--src_root", required=True, type=str)
+    parser.add_argument("--num_pairs", default=2000, type=int, help="Number of pictures")
+
+    return parser.parse_args()
+
+args = parse_args()
+
+src_root = Path(args.src_root)
+src_root = src_root / "train"
+dst_root = Path("data")
+
+# 创建目标目录
+(dst_root / "uav").mkdir(parents=True, exist_ok=True)
+(dst_root / "sat").mkdir(parents=True, exist_ok=True)
+
+# 处理无人机图片
+drone_dir = src_root / "drone"
+
+num_pairs = args.num_pairs
+cur_uav = 0
+for folder in sorted(drone_dir.iterdir()):
+    if cur_uav == num_pairs:
+        break
+    cur_uav = cur_uav + 1
+
+    first_img = next(p for p in folder.iterdir())   # 取排序后的第一张
+    dst_name = f"{cur_uav:04d}.jpg"
+    dst_path = dst_root / "uav" / dst_name
+    shutil.copy2(first_img, dst_path)   # copy2 保留元数据
+
+# 处理卫星图片
+sat_dir = src_root / "satellite"
+
+cur_sat = 0
+for folder in sorted(sat_dir.iterdir()):
+    if cur_sat == num_pairs:
+        break
+    cur_sat = cur_sat + 1
+
+    first_img = next(p for p in folder.iterdir())   # 取第一个
+    dst_name = f"{cur_sat:04d}.jpg"
+    dst_path = dst_root / "sat" / dst_name
+    shutil.copy2(first_img, dst_path)
